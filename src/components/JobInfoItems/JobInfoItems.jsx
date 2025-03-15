8266c0b9c1f9ec1f1120ef03feeb608b41be5fdd
manishsharmacodex <manishsharmadeveloper@gmail.com>
2025-03-15 12:59:12 +0530

    ok

---
+++ src/components/JobInfoItems/JobInfoItems.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './JobInfoItems.css'
+
+const JobInfoItems = () => {
+  return (
+    <div className='jobinfoitems'>
+        <div className="job-info">
+            <p>#India No's 1 Job Portal</p>
+        </div>
+    </div>
+  )
+}
+
+export default JobInfoItems
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Home.css'
+import JobInfoItems from '../../components/JobInfoItems/JobInfoItems'
 
 const Home = () => {
   return (
     <div className='home'>
-      <h1>HOME</h1>
+        <JobInfoItems/>
     </div>
   )
 }
