80aeab2963df0cc55203819fccb2e3e722437983
manishsharmacodex <manishsharmadeveloper@gmail.com>
2025-03-15 14:54:01 +0530

    ok

--- src/components/JobInfoSection/JobInfoSection.jsx
+++ src/components/JobInfoSection/JobInfoSection.jsx
@@ -6,11 +6,14 @@ const JobInfoSection = () => {
     <div className='jobinfoitems'>
         <div className="job-info">
             <p># India No's 1 Job Portal</p>
-            <h1>We Are Hiring...</h1>
+            <h1>We Are Hiring You</h1>
         </div>
         <div className="search-job">
           <label>Search Job's Near By You</label>
-          <input type="text" placeholder='Search Job' />
+          <div className="search-box">
+            <input type="text" placeholder='Search Job' />
+            <button>Search</button>
+          </div>
         </div>
     </div>
   )
