18ffcbbe612acdf1b0a2587febd943648a7d2d4b
manishsharmacodex <manishsharmadeveloper@gmail.com>
2025-03-15 15:31:19 +0530

    ok

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import JobInfoSection from '../../components/JobInfoSection/JobInfoSection'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 const Home = () => {
   return (
     <div className='home'>
         <JobInfoSection/>
+        <AppDownload/>
     </div>
   )
 }
