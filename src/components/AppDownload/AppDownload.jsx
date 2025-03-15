08d9fde697a23694101f93524060e1bd6114ade4
manishsharmacodex <manishsharmadeveloper@gmail.com>
2025-03-15 15:40:32 +0530

    ok

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -5,6 +5,10 @@ const AppDownload = () => {
   return (
     <div className='app-download'>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas non laborum maiores laudantium, praesentium odit</p>
+        <div className="job-btn-box">
+            <button>Download</button>
+            <button>Apply Job's</button>
+        </div>
     </div>
   )
 }
