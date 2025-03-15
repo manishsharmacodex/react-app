043171d9fddb0feac287daf2c794323b19f8cda5
manishsharmacodex <manishsharmadeveloper@gmail.com>
2025-03-15 22:11:10 +0530

    ok

--- src/components/JobInfoSection/JobInfoSection.jsx
+++ src/components/JobInfoSection/JobInfoSection.jsx
@@ -15,6 +15,13 @@ const JobInfoSection = () => {
             <button>Search</button>
           </div>
         </div>
+        <div className='app-download'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas non laborum maiores laudantium, praesentium odit</p>
+        <div className="job-btn-box">
+            <button>Download App</button>
+            <button>Apply Job's</button>
+        </div>
+    </div>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import './Home.css'
 import JobInfoSection from '../../components/JobInfoSection/JobInfoSection'
-import AppDownload from '../../components/AppDownload/AppDownload'
 import JobSection from '../../components/JobSection/JobSection'
 
 const Home = () => {
   return (
     <div className='home'>
         <JobInfoSection/>
-        <AppDownload/>
         <JobSection/>
     </div>
   )
