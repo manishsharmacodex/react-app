8758f55464aadd1a74df33fc48a038dd08594dfe
manishsharmacodex <manishsharmadeveloper@gmail.com>
2025-03-15 13:26:29 +0530

    ok

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
+import JobInfoItems from '../../components/JobInfoItems/JobInfoItems'
 
 const Home = () => {
   return (
     <div className='home'>
         <h1>Manish</h1>
+        <JobInfoItems/>
     </div>
   )
 }
