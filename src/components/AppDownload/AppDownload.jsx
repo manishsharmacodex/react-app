8fa5d654420f2a26fa6cb65312a24783e78aa58d
manishsharmacodex <manishsharmadeveloper@gmail.com>
2025-03-15 15:33:08 +0530

    ok

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -4,7 +4,7 @@ import './AppDownload.css'
 const AppDownload = () => {
   return (
     <div className='app-download'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas non laborum maiores laudantium, praesentium odit culpa fuga, minima sit alias eligendi ab tenetur. Incidunt perspiciatis, cumque magni omnis saepe laudantium!
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas non laborum maiores laudantium, praesentium odit culpa fuga</p>
     </div>
   )
 }
