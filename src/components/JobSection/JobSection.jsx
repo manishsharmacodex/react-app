2a06ead465976081f34d78f650b41645f7300332
manishsharmacodex <manishsharmadeveloper@gmail.com>
2025-03-15 22:15:26 +0530

    ok

--- src/components/JobSection/JobSection.jsx
+++ src/components/JobSection/JobSection.jsx
@@ -4,7 +4,10 @@ import './JobSection.css'
 const JobSection = () => {
   return (
     <div className='job-section'>
-        <h1 className='job-title'>Job's Fillter</h1>
+        <h1 className='job-title'>Top Rated Job's</h1>
+        <div className="jobs-card-box">
+          
+        </div>
     </div>
   )
 }
